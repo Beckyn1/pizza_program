02a963de1ac829a5aa4dd51a8d7eac7597cfce2e
Beckyn1 <becky.ngan@marsden.school.nz>
2021-08-04 11:49:02 +1200

    sprint 3, a bit of sprint 4

--- testing.py
+++ testing.py
@@ -1,34 +1,17 @@
-
-def get_string(m):
-    user_input = input(m)
-    return user_input
-
-def menu_pizza(p):
-    for i in range(len(p)):
-        print("{}: ${}".format(p[i][0], p[i][1]))
-
-def main():
-
-    pizza_list = [
-        ["Cheese", 5],
-        ["Pepperoni", 5],
-        ["Hawaiian", 5],
-        ["Meatlovers", 5]
-    ]
-
-    run_program = True
-    while run_program:
-        print("-" * 42)
-        order_choice = get_string("Please select an option =>")
-        if order_choice == "M":
-            menu_pizza(pizza_list)
-            run_program = False
-        elif order_choice == "Q":
-            print("Thank you for choosing Papa's Pizzeria!")
-            run_program = False
-        else:
-            print("Unrecognised entry must be A or R")
+import re
 
 
-if __name__ == "__main__":
-    main()
+def isValid(s):
+    # 1) Begins with 0 or 91
+    # 2) Then contains 7 or 8 or 9.
+    # 3) Then contains 9 digits
+    Pattern = re.compile("(0|91)?[7-9][0-9]{9}")
+    return Pattern.match(s)
+
+    # Driver Code
+    s = "347873923408"
+    if (isValid(s)):
+        print("Valid Number")
+    else:
+        print("Invalid Number")
+
